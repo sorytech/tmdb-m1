8694fab6aac2eee6cafe1f9eace0275012e94571
diddy90 <jeanpaul90@live.fr>
2018-11-02 19:12:52 +0100

    modif app.Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,14 +27,11 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-<<<<<<< HEAD
 import {FormsModule} from '@angular/forms';
 import {PersonneComponent} from './components/personne/personne.component';
 import { ActeurComponent } from './components/acteur/acteur.component';
-=======
-import { ActeurComponent } from './components/acteur/acteur.component';
 
->>>>>>> a8d43427361f7068f9da0b7f62ad48c24d9ee4c8
+
 
 
 const appRoutes: Routes = [
@@ -54,17 +51,7 @@ const appRoutes: Routes = [
         ResearchComponent,
         ListViewComponent,
         FIlmComponent,
-<<<<<<< HEAD
-        PersonneComponent,
-        ActeurComponent,
-=======
-        ActeurComponent
->>>>>>> a8d43427361f7068f9da0b7f62ad48c24d9ee4c8
-    ],
-    imports: [
-        BrowserModule,
         HttpClientModule,
-        AngularFireModule.initializeApp(environment.firebase),
         AngularFireAuthModule,
         AngularFireDatabaseModule,
         RouterModule.forRoot(appRoutes),
