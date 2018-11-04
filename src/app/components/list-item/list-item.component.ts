1bb82f7e3c3b6b520b2a28e93cf268872877ad96
madidia <mamadoudiandiallo05@gmail.com>
2018-11-04 16:26:21 +0100

    validation

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MovieResponse, MovieGenre } from 'src/app/tmdb-data/Movie';
+import { MovieResponse, MovieGenre, Crew, MovieCredits, Cast } from 'src/app/tmdb-data/Movie';
 import { TmdbService } from 'src/app/services/tmdb/tmdb.service';
 
 @Component({
@@ -14,14 +14,22 @@ export class ListItemComponent implements OnInit {
   public current : MovieResponse={};
   
   public genresResults : MovieGenre[];
+  currentFilmCredits: MovieCredits;
+  crew: Crew;
+  casts: Cast[]=[];
 
   constructor(private tmdb: TmdbService) { }
 
   ngOnInit() {
-    this.tmdb.init('384da4d1d38ad08447d757fb4629fa6b') // Clef de TMDB
-      .getMovie(Number(this.film.id))
-      .then((m: MovieResponse) => {
-        this.current = m;       
-      }).catch(err => console.error('Error getting movie:', err));
+    this.tmdb.getMovieDetails(Number(this.film.id))
+    .then(([mr, mc]) => {
+      this.current = mr;
+      this.currentFilmCredits = mc;
+      this.crew = mc.crew.find((elem) => elem.job === 'Director');
+      mc.cast.forEach((cast) => {
+        this.casts.push(cast);
+      });
+      this.casts.splice(3);
+    }).catch(err => console.error('Error getting movie:', err))
   }
 }
